79bc21c74d9399d6c3b0c1f8e196011f87b2ac5a
Siddharth <siddharth6527@gmail.com>
2024-06-05 01:00:44 +0530

    feat: Added catchAsync in voterController

--- controllers/voterController.js
+++ controllers/voterController.js
@@ -1,103 +1,63 @@
 const Voter = require('../models/voterModel');
-// const Tour = require('../models/voterModel');
+const catchAsync = require('../utils/catchAsync');
 const APIFeatures = require('../utils/apiFeatures');
 
-exports.getAllVoters = async (req, res) => {
-  try {
-    // const voters = await Voter.find();
-    // EXECUTE QUERY
-    const features = new APIFeatures(Voter.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
+exports.getAllVoters = catchAsync(async (req, res) => {
+  const features = new APIFeatures(Voter.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-    const voters = await features.query;
+  const voters = await features.query;
 
-    res.status(200).json({
-      status: 'success',
-      results: voters.length,
-      data: { voters },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    results: voters.length,
+    data: { voters },
+  });
+});
 
-exports.getVoter = async (req, res) => {
-  try {
-    const voter = await Voter.findById(req.params.id);
-    // Tour.findOne({_id: req.params.id})
+exports.getVoter = catchAsync(async (req, res) => {
+  const voter = await Voter.findById(req.params.id);
+  // Tour.findOne({_id: req.params.id})
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        voter,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      voter,
+    },
+  });
+});
 
-exports.createVoter = async (req, res) => {
-  // const newTour = new Tour({});
-  // newTour.save();
+exports.createVoter = catchAsync(async (req, res) => {
+  const newVoter = await Voter.create(req.body);
 
-  try {
-    const newVoter = await Voter.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      voter: newVoter,
+    },
+  });
+});
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        voter: newVoter,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Invalid data sent!',
-    });
-  }
-};
+exports.updateVoter = catchAsync(async (req, res) => {
+  const voter = await Voter.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-exports.updateVoter = async (req, res) => {
-  try {
-    const voter = await Voter.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+  res.status(201).json({
+    status: 'success',
+    data: { voter },
+  });
+});
 
-    res.status(201).json({
-      status: 'success',
-      data: { voter },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+exports.deleteVoter = catchAsync(async (req, res) => {
+  await Voter.findByIdAndDelete(req.params.id);
 
-exports.deleteVoter = async (req, res) => {
-  try {
-    await Voter.findByIdAndDelete(req.params.id);
-
-    res.status(204).json({
-      status: 'success',
-      message: null,
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(204).json({
+    status: 'success',
+    message: null,
+  });
+});
