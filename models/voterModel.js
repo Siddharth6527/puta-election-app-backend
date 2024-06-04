e59b6954312959365e3b44b0229890eeab43976f
Siddharth <siddharth6527@gmail.com>
2024-06-05 01:29:45 +0530

    feat: voterModel Schema_updated_1

--- models/voterModel.js
+++ models/voterModel.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const voterSchema = new mongoose.Schema({
+  sno: {
+    type: Number,
+    required: [true, 'A voter must have serial no'],
+  },
   name: {
     type: String,
     required: [true, 'A voter must have a name'],
     trim: true,
     minLength: 5,
+  },
+  designation: {
+    type: String,
+    required: [true, 'A voter must have designation'],
+    minLength: 2,
   },
   college: {
     type: String,
@@ -13,15 +23,41 @@ const voterSchema = new mongoose.Schema({
     trim: true,
     minLength: 5,
   },
-  collegeInitials: {
-    type: String,
-    required: [true, 'A voter must have a college initials'],
-    trim: true,
-    minLength: 2,
+  receiptNo: {
+    type: Number,
+    required: [true, 'A voter must have receipt number'],
   },
   voted: {
     type: Boolean,
     required: [true, 'A voter may or may not have voted'],
+    default: false,
+  },
+  memebershipCategory: {
+    type: String,
+    default: 'General',
+    minLength: 3,
+  },
+  email: {
+    type: String,
+    required: [true, 'A voter must have a email'],
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  password: {
+    type: String,
+    minLength: 8,
+    // select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true],
+    validate: {
+      // this only works on CREATE and SAVE!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
   },
 });
 
