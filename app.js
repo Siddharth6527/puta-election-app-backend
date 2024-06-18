a55bede303b2f9429e8e1542d507d26d2b679bce
Siddharth <siddharth6527@gmail.com>
2024-06-18 21:12:39 +0530

    feat: Document Limit fixed

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const corsOptions = {
-  origin: 'https://puta-election-app-frontend.onrender.com',
-  // origin: 'http://localhost:5173',
+  // origin: 'https://puta-election-app-frontend.onrender.com',
+  origin: 'http://localhost:5173',
   optionsSuccessStatus: 200,
 };
 
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -49,7 +49,7 @@ class APIFeatures {
 
   paginate() {
     const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
+    const limit = this.queryString.limit * 1 || 500;
     const skip = (page - 1) * limit;
     // page=2&limit=10,1-10 page 1, 11-20 page 2, 21-30 page 3
     this.query = this.query.skip(skip).limit(limit);
