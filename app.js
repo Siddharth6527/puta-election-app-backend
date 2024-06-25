f674fe1db3295754a7cd9691e16f621f595753ce
Siddharth <siddharth6527@gmail.com>
2024-06-25 05:40:28 +0530

    feat: Added Custom Deployed URL to CORS

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const corsOptions = {
-  origin: 'https://puta-election-app-frontend.onrender.com',
+  origin: 'www.pantuniversityputa.in',
+  // origin: 'https://puta-election-app-frontend.onrender.com',
   // origin: 'http://localhost:5173',
   optionsSuccessStatus: 200,
 };
